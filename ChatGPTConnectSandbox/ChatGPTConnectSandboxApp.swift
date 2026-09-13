c57f1d037873492eec26001a55ac4be9c72c49ce
Prince <prince@kodlabs.in>
2026-09-13 21:03:53 +0530

    feat: sandbox app setup

---
+++ ChatGPTConnectSandbox/ChatGPTConnectSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTConnectSandboxApp.swift
+//  ChatGPTConnectSandbox
+//
+//  Created by Prince on 13/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTConnectSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTConnectSandbox/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ChatGPTConnectSandbox
+//
+//  Created by Prince on 13/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
